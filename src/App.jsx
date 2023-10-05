077b3280edfe04f805f9e8d6df500d66fa273d35
BrunoNMoura <bnmalemao@gmail.com>
2023-10-04 21:13:40 -0300

    :wrench: :construction: folders pages

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useContext, useEffect } from 'react';
+import { Router } from './router/Router';
+import { LabedditContext } from './global/LabedditContext';
+
+export default function App() {
+
+    const context = useContext(LabedditContext)
+            
+    useEffect(() => {
+        const isHTTPS = window.location.protocol === "https:";
+        if (isHTTPS) {
+            window.location.href = `http://${window.location.host}${window.location.pathname}`;
+        }
+        context.checkLogin()
+    }, [])
+    return (
+        <Router />
+    )
+}
