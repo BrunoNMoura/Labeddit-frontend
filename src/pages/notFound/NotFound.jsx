50c2b571e0927517c48f55fe857cf3837d96fad1
BrunoNMoura <bnmalemao@gmail.com>
2023-10-05 16:10:22 -0300

    :bricks: not found page created

---
+++ src/pages/notFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../signup/styled";
+import { Container } from "./styled";
+import { handleHome } from "../../router/cordinator";
+
+export function NotFound(){
+  const navigate = useNavigate()
+  return(
+    <Container>
+      <h1>404</h1>
+      <br/>
+      <h2>Está página não existe!</h2>
+      <Button onClick={()=>handleHome(navigate)}>Login</Button>
+    </Container>
+  )
+}
